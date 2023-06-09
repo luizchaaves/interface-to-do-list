5382a2ba768e7bc044b7c293ae72c51e92140ec7
Luiz Otavio <luizotavio0106@outlook.com>
2023-06-09 02:01:08 -0300

    socket.io-client configuration to receive a command from the server and update tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import io from 'socket.io-client';
+import { useDispatch } from 'react-redux';
+import { API_URL } from './services/api';
 import Routes from './routes';
 
 import GlobalStyle from './styles/global';
@@ -11,12 +14,28 @@ import { Container } from './styles';
 import Menu from './components/Menu';
 import useMedia from './hooks/useMedia';
 import { useAppSelector } from './hooks/hooks';
-import { RootState } from './store/configureStore';
+
+import { AppDispatch, RootState } from './store/configureStore';
+import { tasksGet } from './store/tasks';
+import { countTasksGet } from './store/coutTasks';
+import { finishedTasksGet } from './store/finishedTasks';
+import { openedTasksGet } from './store/openedTasks';
 
 function App() {
   const [isDarkTheme, setIsDarkTheme] = useState(true);
   const { theme } = useAppSelector((state: RootState) => state.theme);
   const mobile = useMedia('(max-width: 600px)');
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    const socket = io(API_URL);
+    socket.on('clearTasks', () => {
+      dispatch<any>(tasksGet(undefined));
+      dispatch<any>(finishedTasksGet(undefined));
+      dispatch<any>(openedTasksGet(undefined));
+      dispatch<any>(countTasksGet(undefined));
+    });
+  }, [dispatch]);
 
   useEffect(() => {
     setIsDarkTheme(theme === 'dark');
